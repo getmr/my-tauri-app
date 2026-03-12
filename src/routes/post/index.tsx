26766503830512e821e74cb58199fe26f4db08ad
zjt <zhangjintao@zmexing.com>
2026-03-12 14:24:46 +0800

    headless ui

--- src/routes/post/index.tsx
+++ src/routes/post/index.tsx
@@ -1,9 +1,115 @@
+'use client'
+
 import { createFileRoute } from '@tanstack/react-router'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 
 export const Route = createFileRoute('/post/')({
   component: RouteComponent,
 })
 
+
+const categories = [
+    {
+      name: 'Recent',
+      posts: [
+        {
+          id: 1,
+          title: 'Does drinking coffee make you smarter?',
+          date: '5h ago',
+          commentCount: 5,
+          shareCount: 2,
+        },
+        {
+          id: 2,
+          title: "So you've bought coffee... now what?",
+          date: '2h ago',
+          commentCount: 3,
+          shareCount: 2,
+        },
+      ],
+    },
+    {
+      name: 'Popular',
+      posts: [
+        {
+          id: 1,
+          title: 'Is tech making coffee better or worse?',
+          date: 'Jan 7',
+          commentCount: 29,
+          shareCount: 16,
+        },
+        {
+          id: 2,
+          title: 'The most innovative things happening in coffee',
+          date: 'Mar 19',
+          commentCount: 24,
+          shareCount: 12,
+        },
+      ],
+    },
+    {
+      name: 'Trending',
+      posts: [
+        {
+          id: 1,
+          title: 'Ask Me Anything: 10 answers to your questions about coffee',
+          date: '2d ago',
+          commentCount: 9,
+          shareCount: 5,
+        },
+        {
+          id: 2,
+          title: "The worst advice we've ever heard about coffee",
+          date: '4d ago',
+          commentCount: 1,
+          shareCount: 2,
+        },
+      ],
+    },
+  ]
+
+
 function RouteComponent() {
-  return <div className="text-gray-500">Hello "/post/"!</div>
+  return (
+    <div className="flex h-screen w-full justify-start px-4 pt-5 bg-gray-700 text-gray-800 rounded-3xl">
+      <div className="w-full max-w-md">
+        <TabGroup vertical={false}>
+          <TabList className="flex gap-4">
+            {categories.map(({ name }) => (
+              <Tab
+                key={name}
+                className="rounded-full px-3 py-1 text-sm/6 font-semibold text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-white/5 data-selected:bg-white/10 data-selected:data-hover:bg-white/10"
+              >
+                {name}
+              </Tab>
+            ))}
+          </TabList>
+          <TabPanels className="mt-3">
+            {categories.map(({ name, posts }) => (
+              <TabPanel key={name} className="rounded-xl bg-white/5 p-3">
+                <ul>
+                  {posts.map((post) => (
+                    <li key={post.id} className="relative rounded-md p-3 text-sm/6 transition hover:bg-white/5">
+                      <a href="#" className="font-semibold text-white">
+                        <span className="absolute inset-0" />
+                        {post.title}
+                      </a>
+                      <ul className="flex gap-2 text-white/50" aria-hidden="true">
+                        <li>{post.date}</li>
+                        <li aria-hidden="true">&middot;</li>
+                        <li>{post.commentCount} comments</li>
+                        <li aria-hidden="true">&middot;</li>
+                        <li>{post.shareCount} shares</li>
+                      </ul>
+                    </li>
+                  ))}
+                </ul>
+              </TabPanel>
+            ))}
+          </TabPanels>
+        </TabGroup>
+      </div>
+    </div>
+  )
 }
+
--- src/routes/post/route.tsx
+++ src/routes/post/route.tsx
@@ -6,14 +6,5 @@ export const Route = createFileRoute("/post")({
 });
 
 function RouteComponent() {
-  return (
-    <div className="flex justify-center rounded-lg">
-      <div className="p-[1.5px] rounded-full bg-linear-to-r from-pink-500 via-purple-500 to-cyan-400 shadow-lg">
-        <div className="flex gap-6 px-2 py-2 rounded-full bg-gray-900 items-center">
-          <h1 className="text-4xl font-bold text-gray-800">Post Layout</h1>
-          <Outlet />
-        </div>
-      </div>
-    </div>
-  );
+  return <Outlet />;
 }
